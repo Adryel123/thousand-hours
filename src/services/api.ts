ac828e4f423fe23152356a6531502bc98f4a939c
adryel123 <adryel.pnp@gmail.com>
2021-11-28 22:57:55 -0300

    update baseurl to use HTTPS

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://thousandhours.pythonanywhere.com'
+  baseURL: 'https://thousandhours.pythonanywhere.com'
 })
 
 export default api
